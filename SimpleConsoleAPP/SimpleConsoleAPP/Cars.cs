42d545ee91d1c237ba568e032d58f8c2b18838ee
Seth Kigen <kigen@outlook.com>
2017-05-25 19:02:59 +0300

    A simple console app to demo using c# classes to model your entities

---
+++ SimpleConsoleAPP/SimpleConsoleAPP/Cars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleConsoleAPP
+{
+     class Cars
+    {
+
+         public Cars()
+         {
+             seats = 4;
+         }
+         public string Make { get; set; }
+         public string Model { get; set; }
+         public int YearOfManufacture { get; set; }
+
+         private int seats;
+         public int Seats
+         {
+             get { return seats; }
+         }
+
+         public void PrintReport()
+         {
+             //prints out a report about the car. 
+             Console.WriteLine("The car Make is:{0}", this.Make);
+             Console.WriteLine("The car Model is:{0}", this.Model);
+             Console.WriteLine("The car was manaufactured in {0}", this.YearOfManufacture);
+         }
+    }
+}
---
+++ SimpleConsoleAPP/SimpleConsoleAPP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name");
+            string myName =  Console.ReadLine();
+            Console.WriteLine("Your name is {0}", myName);
+        
+            //OOP.
+            // - Classes (java) look at C#
+            Cars car = new Cars();
+            Console.WriteLine("What is the model of your car.");
+            car.Make = Console.ReadLine();
+            Console.WriteLine("What is the make of your car.");
+            car.Model = Console.ReadLine();
+            Console.WriteLine("Which year was your car manufatured");
+            car.YearOfManufacture = int.Parse(Console.ReadLine());
+
+            car.PrintReport();
+
+            Console.ReadLine();
+        }
+    }
+}
