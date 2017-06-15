202dd7353554f2391b5be6e1db445bde6c7738fb
Seth Kigen <kigen@outlook.com>
2017-06-15 19:59:31 +0300

    Added useful links

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# WindowsMobileClass2017April
+# Windows Mobile Programming:  2017 
+
+
+## Useful Links: 
+
+- Visual Studio 2015 Community Edition.: https://gist.github.com/CHEF-KOCH/8078b39e1aad71ef6c317a3c0edb6ec9
+- Previous class Examples:  https://github.com/kigen/WindowsPhoneClass2016Oct
+
---
+++ TODO.APP/MyTask/TheTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.ViewManagement;
+
+namespace MyTask
+{
+    public sealed class TheTask:IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+           //This is where you write your task code. 
+            int a = 10, b=20;
+            int z = a + b;
+            //Write to file... 
+        }
+    }
+}
